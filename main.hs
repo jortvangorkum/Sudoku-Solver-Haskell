32ca720c97e106c1f570eb611992da4d0545f4c7
Jort van Gorkum <jortvangorkum@gmail.com>
2018-09-24 20:38:15 +0200

    TODO: update value from field

--- main.hs
+++ main.hs
@@ -3,6 +3,7 @@ module Main where
 import Data.Char
 import Data.List
 import GHC.Exts
+import Debug.Trace
 
 
 -- | Model
@@ -39,13 +40,26 @@ main = interact solver where
             printField :: Field -> String
             printField (value, pValues, x, y) = [intToDigit value]
         solveSudoku :: Sudoku -> Sudoku
-        solveSudoku sudoku = unconcat (solve (concat sudoku))
+        solveSudoku sudoku = unconcat (solve [] (concat sudoku))
             where 
-            solve :: [Field] -> [Field]
-            solve sudoku = sudoku
             unconcat :: [Field] -> [[Field]]
-            unconcat = groupWith (\(v, pV, x, y) -> y) 
+            unconcat = groupWith (\(v, pVs, x, y) -> y) 
+            solve :: [Field] -> [Field] -> [Field]
+            solve done [] = done
+            solve done notdone@(x:xs)
+                | possibleValues x (done ++ notdone) = solve (done ++ [x]) xs
+                | otherwise = solve (init done) (map (\(v, pVs, x, y) -> (v, [0..9], x, y)) (last done : notdone)) 
                 where
-                possibleValues :: Field
-                possibleValues = undefined
+                possibleValues :: Field -> [Field] -> Bool
+                possibleValues (v, [], x, y) fields = False
+                possibleValues (v, pV:pVs, x, y) fields
+                    | check (pV, pVs, x, y) fields = True
+                    | otherwise = possibleValues (v, pVs, x, y) fields
+                    where
+                    check :: Field -> [Field] -> Bool
+                    check (v, _, x, y) [] = True
+                    check field@(v, _, x, y) ((v2, _, x2, y2):xs)
+                        -- Check Row Column and Plane
+                        | v == v2 && ((x == x2) || (y == y2) || (x`div`3 == x2`div`3 && y`div`3==y2`div`3)) = False
+                        | otherwise = trace ("v:" ++ show v ++ " x:" ++ show x ++ " y:" ++ show y) $ check field xs
         
