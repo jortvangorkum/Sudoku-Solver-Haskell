2e51d52c84dfe07e267df7d8ce72506c07e492ab
jortvangorkum <jortvangorkum@gmail.com>
2018-10-11 21:18:16 +0200

    Cleanup

--- main.hs
+++ main.hs
@@ -49,10 +49,7 @@ printSudoku :: Sudoku -> String
 printSudoku [row] = printRow row
 printSudoku (row@(field@(_, _, _, y):fields):rows)
     | (y+1) `mod` 3 == 0 = printRow row ++ printLine ++ printSudoku rows
-    | otherwise = printRow row ++ "\n" ++ printSudoku rows
-
-showField :: Field -> String
-showField (v, pVs, x, y) = "(v:" ++ show v ++ " pVs:" ++ show pVs ++ " x:" ++ show x ++ " y:" ++ show y ++ ")"
+    | otherwise          = printRow row ++ "\n" ++ printSudoku rows
 
 unconcatFields :: [Field] -> Sudoku
 unconcatFields = groupWith (\(v, pVs, x, y) -> y)
@@ -61,40 +58,37 @@ unconcatFields = groupWith (\(v, pVs, x, y) -> y)
     Solving Sudoku
 -}
 
--- resetField :: Field -> Field
--- resetField (v, [], x, y) = (v, [1..9], x, y)
-
 replaceFields :: [Field] -> [Field] -> [Field]
 replaceFields [] xs = xs
 replaceFields _ []  = []
-replaceFields replace@(rf@(_,_,rx,ry):rfs) fields@(f@(_,_,fx,fy):fs)
+replaceFields rp@(rf@(_,_,rx,ry):rfs) (f@(_,_,fx,fy):fs)
     | rx == fx && ry == fy = rf : replaceFields rfs fs
-    | otherwise = f : replaceFields replace fs
+    | otherwise            = f : replaceFields rp fs
 
 emptyFields :: [Field] -> [Field]
 emptyFields = filter (\(v, _, _, _) -> v == 0)
 
 checkField :: Field -> [Field] -> Bool
 checkField _ [] = True
-checkField field@(v, _, x, y) ((v2, _, x2, y2):xs)
+checkField f@(v, _, x, y) (f2@(v2, _, x2, y2):xs)
+    | check f f2 = False
+    | otherwise  = checkField f xs
+    where
     -- Check Row Column and Plane
-    | v == v2 && ((x == x2) || (y == y2) || ((x`div`3) == (x2`div`3) && (y`div`3) == (y2`div`3))) = False
-    | otherwise = checkField field xs
+    check :: Field -> Field -> Bool
+    check (v, _, x, y) (v2, _, x2, y2) = v == v2 && ((x == x2) || (y == y2) || ((x`div`3) == (x2`div`3) && (y`div`3) == (y2`div`3)))
 
 loopThroughpValues :: Field -> [Field] -> Field
-loopThroughpValues field@(_, _, x, y) fields = (0, [1..9], x, y)
+loopThroughpValues (_, [], x, y) fields = (0, [1..9], x, y)
 loopThroughpValues (v, pV:pVs, x, y) fields
     | checkField (pV, pVs, x, y) fields = (pV, pVs, x, y)
-    | otherwise = loopThroughpValues (v, pVs, x, y) fields
+    | otherwise                         = loopThroughpValues (v, pVs, x, y) fields
 
 solveEmptyFields :: [Field] -> [Field] -> [Field] -> [Field]
 solveEmptyFields sudoku done [] = replaceFields done sudoku
 solveEmptyFields sudoku done notdone@(x:xs) = case loopThroughpValues x (replaceFields done sudoku) of
-    field@(0, _, _, _)   -> solveEmptyFields sudoku (init done) (last done : field : xs)
-    field                -> solveEmptyFields sudoku (done ++ [field]) xs
-    where
-    trace1 field done (notdone:(notdone2:_)) = trace ("V1:" ++ showField field ++ " NotDone1:" ++ showField notdone ++ " NotDone1:" ++ showField notdone2)
-    trace2 field done (notdone:(notdone2:_)) = trace ("V2:" ++ showField field ++ " NotDone2:" ++ showField notdone ++ " NotDone2:" ++ showField notdone2)
+    f@(0, _, _, _) -> solveEmptyFields sudoku (init done) (last done : f : xs)
+    f              -> solveEmptyFields sudoku (done ++ [f]) xs
 
 solveSudoku :: Sudoku -> Sudoku
 solveSudoku sudoku = unconcatFields (solveEmptyFields fields [] (emptyFields fields))
@@ -108,7 +102,7 @@ solveSudoku sudoku = unconcatFields (solveEmptyFields fields [] (emptyFields fie
 
 main = interact solver where
     solver :: String -> String
-    solver input = printSudoku (solveSudoku (parseSudoku input)) ++ "\n"
+    solver = printSudoku . solveSudoku . parseSudoku
 
 
 {-
@@ -127,7 +121,6 @@ testSudoku = [(0,[1..9],0,0),(4,[1..9],1,0),(0,[1..9],2,0),(0,[1..9],3,0),(0,[1.
               (9,[1..9],0,8),(2,[1..9],1,8),(8,[1..9],2,8),(0,[1..9],3,8),(0,[1..9],4,8),(0,[1..9],5,8),(0,[1..9],6,8),(6,[1..9],7,8),(0,[1..9],8,8)]
 
 tests = TestList [
-    "test resetField"                           ~: (0, [1..9], 0, 0)                    ~=? resetField (0, [], 0, 0),
     "test checkFieldH"                          ~: False                                ~=? checkField (1, [], 0, 0) testSudoku,
     "test checkFieldV"                          ~: False                                ~=? checkField (3, [], 0, 0) testSudoku,
     "test checkFieldP"                          ~: False                                ~=? checkField (2, [], 0, 0) testSudoku,
